c2a638f009d8d1f8dca6681210e29eccb93d1b8a
Gurdeep Singh Saini <gurdeeprocksman2@gmail.com>
2020-12-06 16:17:44 +0530

    feat: added reset button refactor: switch position changed

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const switchToggle = document.querySelector(".switch-input")
 const input = document.querySelector(".linput")
 const output = document.querySelector(".rinput")
 const translateBtn = document.querySelector(".translateBtn")
+const resetBtn = document.querySelector('.reset')
 const baseUrl = 'https://api.funtranslations.com/translate/minion.json'
 
-switchToggle.addEventListener('change', (e) => {
+
+
+const reset = () => {
+  input.value = ''
+  output.value = ''
+}
+
+const translate = () => {
+  let encoded = `${baseUrl}?text=${encodeURI(input.value)}`
+  fetch(encoded)
+    .then(response => response.json())
+    .then(json => output.value = json.contents.translated)
+    .catch(e => {
+      alert('error: either you\'re offline or the api request limit has been reached. Kindly switch to Turbo Mode by clicking on the toggle to have uninterrupted translation.')
+      console.log(e.message)
+    })
+}
+
+const switchHandler = (e) => {
   reset()
   let bool = e.target.checked
   if (bool) {
@@ -16,22 +35,10 @@ switchToggle.addEventListener('change', (e) => {
     translateBtn.style.display = ''
     unmount_watch(bool)
   }
-})
+}
+
+switchToggle.addEventListener('change', switchHandler )
 
 translateBtn.addEventListener('click', translate)
 
-function reset() {
-  input.value = ''
-  output.value = ''
-}
-
-function translate() {
-  let encoded = `${baseUrl}?text=${encodeURI(input.value)}`
-  fetch(encoded)
-    .then(response => response.json())
-    .then(json => output.value = json.contents.translated)
-    .catch(e => {
-      alert('error: either you\'re offline or the api request limit has bee reached. Kindly switch to Turbo Mode by clicking on the toggle to have uninterrupted translation.')
-      console.log(e.message)
-    })
-}
+resetBtn.addEventListener('click', reset)
